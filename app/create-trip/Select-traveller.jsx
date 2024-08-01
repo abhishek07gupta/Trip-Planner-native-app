65f6d523a46f482edcde737e91d51cbb10273271
Abhishek gupta <07abhishekgupta07@gmail.com>
2024-08-02 00:54:27 +0530

    udated select travellers

--- app/create-trip/Select-traveller.jsx
+++ app/create-trip/Select-traveller.jsx
@@ -1,6 +1,9 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, FlatListComponent } from 'react-native'
 import React, { useEffect } from 'react'
 import { useNavigation } from 'expo-router'
+import {Colors} from './../../constants/Colors'
+import {SelectTravelersList} from './../../constants/Options'
+import OptionCard from '../../components/CreateTrip/OptionCard'
 
 export default function SelectTraveller() {
     const navigation = useNavigation();
@@ -8,15 +11,34 @@ export default function SelectTraveller() {
         navigation.setOptions({
             headerShown:true,
             headerTransparent:true,
-            headerTitle:'Select Travllers'
+            headerTitle:''
         })
     },[])
   return (
     <View style={{
         padding:25,
-        paddingTop:100
+        paddingTop:100,
+        height:'100%',
+        backgroundColor:Colors.WHITE
+
     }}>
-      <Text>SelectTravaller</Text>
-    </View>
+      <Text style={{
+        fontSize:35,
+        fontFamily:'outfit-bold',
+        marginTop:5
+      }}>Who's Travelling</Text>
+      <View style={{marginTop:10}}>
+        <Text style={{fontFamily:'outfit-bold', fontSize:25}}>Select your Travellers</Text>
+        <FlatList data={SelectTravelersList}
+        keyExtractor={item => item.id}
+        renderItem={(item,index)=>(
+          <View>
+            <OptionCard option={item}/>
+          </View>
+  )}>
+          
+        </FlatList>
+      </View>
+          </View>
   )
 }
---
+++ constants/Options.js
@@ -0,0 +1,31 @@
+export const SelectTravelersList=[
+    {
+        id:1,
+        title:'Solo',
+        desc:'Solo traveller in exploration',
+        count:'1',
+        icon:' '
+    },
+    {
+        id:2,
+        title:'Couple',
+        desc:'A amazing trip with your partner , your love and crime mate',
+        count:'2',
+        icon:' '
+    },
+    {
+        id:3,
+        title:'Family',
+        desc:'Complete package of wildness and fun',
+        count:'3 to 5',
+        icon:' '
+    },
+    {
+        id:4,
+        title:'Friends',
+        desc:'Some time to reconnect with your friends',
+        count:'5 to 10',
+        icon:' '
+    }
+]
+
